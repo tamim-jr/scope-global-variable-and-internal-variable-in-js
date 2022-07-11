4923e19096bdad8bf82b0210a6e0cd721005d8b5
Tamim Mahmud <internationalinf24@gmail.com>
2022-07-11 22:37:29 +0600

    Global variable and internal variable in JavaScript

---
+++ scope.js
@@ -0,0 +1,14 @@
+
+allNames(15, 45)
+function allNames(nam1, nam2){
+    const result = nam1 + nam2;
+    let newResult = result;
+    console.log(newResult);
+}
+
+let allName= [15, 75, 95, 15, 12];
+
+let myName =  allName.map(x => x + x);
+console.log(myName);
+
+//global variable and internal veritable in javascript
